94e5ffb8518169b4e2ba5a008fae432cfd4e6e6c
Yu Huang <plasmaclanreboot@163.com>
2026-01-22 22:28:30 +0800

    Unet test realization

--- src/nn_utils.py
+++ src/nn_utils.py
@@ -12,9 +12,10 @@ Revision:
 [Date]         [By]         [Version]         [Change Log]\n
 2026.1.21      Yu Huang     1.0               First implementation\n
 2026.1.22      Yu Huang     1.1               Unet debug\n
+2026.1.22      Yu Huang     1.2               PDN generator & dumper realization\n
 
 Details:
-NN dataset, dataloader, optimizer, loss function definitions.
+NN dataset, dataloader, optimizer, loss function definitions and other utilities such as PDN generator and dumper.
 ------------------------------------------------------------------------------------------------------------------------
 
 """
@@ -109,3 +110,171 @@ def numpy_to_mat(data_in: np.array, path: str, name='data'):
     """save numpy vars to mat"""
     data = {name: data_in}
     sio.savemat(path, data)
+
+
+def gen_pdn(sel: int, p_height: int, p_width: int, min_seg_num: int, max_seg_num: int, pdn_voltage: float):
+    """Gen PDN with given params, specific to edge-only"""
+    edge_sel = f'{sel:0{4}b}'  # select code of edge [0]->[Left, Right, Top, Down]<-[3]
+    seg_num_arr = [0, 0, 0, 0]  # segment amount of edge [Left, Right, Top, Down]
+    seg_len_arr = [0, 0, 0, 0]  # segment length of edge [Left, Right, Top, Down]
+    # left edge is selected
+    if edge_sel[0] == '1':
+        seg_num = np.random.randint(min_seg_num, max_seg_num + 1, 1)
+        seg_edge = np.random.choice(p_height, size=2 * seg_num, replace=False)
+        seg_edge.sort()
+        row_left = None
+        for k in range(seg_num[0]):
+            if k == 0:
+                row_left = np.linspace(start=seg_edge[2 * k], stop=seg_edge[2 * k + 1],
+                                       num=seg_edge[2 * k + 1] - seg_edge[2 * k] + 1,
+                                       endpoint=True, dtype=np.int32)
+            else:
+                row_left = np.hstack((row_left, np.linspace(start=seg_edge[2 * k], stop=seg_edge[2 * k + 1],
+                                                            num=seg_edge[2 * k + 1] - seg_edge[2 * k] + 1,
+                                                            endpoint=True, dtype=np.int32)))
+        col_left = np.zeros((1, row_left.size), dtype=np.int32)
+        col_left = col_left[0]
+        seg_num_arr[0] = seg_num[0]
+        seg_len_arr[0] = row_left.size
+    else:
+        row_left = None
+        col_left = None
+        seg_num_arr[0] = 0
+        seg_len_arr[0] = 0
+
+    # right edge is selected
+    if edge_sel[1] == '1':
+        seg_num = np.random.randint(min_seg_num, max_seg_num + 1, 1)
+        seg_edge = np.random.choice(p_height, size=2 * seg_num, replace=False)
+        seg_edge.sort()
+        row_right = None
+        for k in range(seg_num[0]):
+            if k == 0:
+                row_right = np.linspace(start=seg_edge[2 * k], stop=seg_edge[2 * k + 1],
+                                        num=seg_edge[2 * k + 1] - seg_edge[2 * k] + 1,
+                                        endpoint=True, dtype=np.int32)
+            else:
+                row_right = np.hstack(
+                    (row_right, np.linspace(start=seg_edge[2 * k], stop=seg_edge[2 * k + 1],
+                                            num=seg_edge[2 * k + 1] - seg_edge[2 * k] + 1,
+                                            endpoint=True, dtype=np.int32)))
+        col_right = np.zeros((1, row_right.size), dtype=np.int32) + p_width - 1
+        col_right = col_right[0]
+        seg_num_arr[1] = seg_num[0]
+        seg_len_arr[1] = row_right.size
+    else:
+        row_right = None
+        col_right = None
+        seg_num_arr[1] = 0
+        seg_len_arr[1] = 0
+
+    # Top edge is selected
+    if edge_sel[2] == '1':
+        seg_num = np.random.randint(min_seg_num, max_seg_num + 1, 1)
+        seg_edge = np.random.choice(p_width, size=2 * seg_num, replace=False)
+        seg_edge.sort()
+        col_top = None
+        for k in range(seg_num[0]):
+            if k == 0:
+                col_top = np.linspace(start=seg_edge[2 * k], stop=seg_edge[2 * k + 1],
+                                      num=seg_edge[2 * k + 1] - seg_edge[2 * k] + 1,
+                                      endpoint=True, dtype=np.int32)
+            else:
+                col_top = np.hstack((col_top, np.linspace(start=seg_edge[2 * k], stop=seg_edge[2 * k + 1],
+                                                          num=seg_edge[2 * k + 1] - seg_edge[2 * k] + 1,
+                                                          endpoint=True, dtype=np.int32)))
+        row_top = np.zeros((1, col_top.size), dtype=np.int32)
+        row_top = row_top[0]
+        seg_num_arr[2] = seg_num[0]
+        seg_len_arr[2] = col_top.size
+    else:
+        row_top = None
+        col_top = None
+        seg_num_arr[2] = 0
+        seg_len_arr[2] = 0
+
+    # Down edge is selected
+    if edge_sel[3] == '1':
+        seg_num = np.random.randint(min_seg_num, max_seg_num + 1, 1)
+        seg_edge = np.random.choice(p_width, size=2 * seg_num, replace=False)
+        seg_edge.sort()
+        col_down = None
+        for k in range(seg_num[0]):
+            if k == 0:
+                col_down = np.linspace(start=seg_edge[2 * k], stop=seg_edge[2 * k + 1],
+                                       num=seg_edge[2 * k + 1] - seg_edge[2 * k] + 1,
+                                       endpoint=True, dtype=np.int32)
+            else:
+                col_down = np.hstack((col_down, np.linspace(start=seg_edge[2 * k], stop=seg_edge[2 * k + 1],
+                                                            num=seg_edge[2 * k + 1] - seg_edge[2 * k] + 1,
+                                                            endpoint=True, dtype=np.int32)))
+        row_down = np.zeros((1, col_down.size), dtype=np.int32) + p_height - 1
+        row_down = row_down[0]
+        seg_num_arr[3] = seg_num[0]
+        seg_len_arr[3] = col_down.size
+    else:
+        row_down = None
+        col_down = None
+        seg_num_arr[3] = 0
+        seg_len_arr[3] = 0
+
+    # merge arrays
+    pdn_row = np.hstack([x for x in [row_left, row_right, row_top, row_down] if x is not None])
+    pdn_col = np.hstack([y for y in [col_left, col_right, col_top, col_down] if y is not None])
+    pdn_vol = np.zeros((1, pdn_row.size), dtype=np.float32) + pdn_voltage
+    pdn_vol = pdn_vol[0]
+    amount = pdn_row.size
+    if pdn_row.size != pdn_col.size:
+        sys_log.error("Unmatched row with %d elements and col with %d elements", pdn_row.size, pdn_col.size)
+        sys.exit(0)
+    if sum(seg_len_arr) != pdn_row.size:
+        sys_log.error("Unmatched sum of segments with %d elements and row with %d elements",
+                      sum(seg_len_arr), pdn_row.size)
+        sys.exit(0)
+    return seg_num_arr, seg_len_arr, amount, pdn_row, pdn_col, pdn_vol
+
+
+def dump_pdn(file_path: str, column: int, precision: int, amount: int,
+             pdn_row: np.ndarray, pdn_col: np.ndarray, pdn_vol: np.ndarray):
+    """Dump the PDN into json file without json lib"""
+    with open(file_path, "w") as fid:
+        fid.write('{\n    "amount": %d,\n' % amount)
+        # Export row
+        cur_col = 1
+        fid.write('    "row": [')
+        for ii in range(amount):
+            if cur_col > column:
+                cur_col = 1
+                fid.write("\n    ")
+            if ii != amount - 1:
+                fid.write('%d, ' % int(pdn_row[ii]))
+            else:
+                fid.write('%d],\n' % int(pdn_row[ii]))
+            cur_col += 1
+
+        # Export col
+        cur_col = 1
+        fid.write('    "col": [')
+        for ii in range(amount):
+            if cur_col > column:
+                cur_col = 1
+                fid.write("\n    ")
+            if ii != amount - 1:
+                fid.write('%d, ' % int(pdn_col[ii]))
+            else:
+                fid.write('%d],\n' % int(pdn_col[ii]))
+            cur_col += 1
+
+        # Export voltage
+        cur_col = 1
+        fid.write('    "voltage": [')
+        for ii in range(amount):
+            if cur_col > column:
+                cur_col = 1
+                fid.write("\n    ")
+            if ii != amount - 1:
+                fid.write('%.{}e, '.format(precision) % pdn_vol[ii])
+            else:
+                fid.write('%.{}e]\n'.format(precision) % pdn_vol[ii])
+            cur_col += 1
+        fid.write('}\n')
--- src/unet_model.py
+++ src/unet_model.py
@@ -11,27 +11,38 @@ Revision:
 ------------------------------------------------------------------------------------------------------------------------
 [Date]         [By]         [Version]         [Change Log]\n
 2026.1.21      Yu Huang     1.0               First implementation\n
+2026.1.22      Yu Huang     1.1               Unet test realization\n
 
 Details:
 Training and inference pipeline of the Unet and DDIM model.
 ------------------------------------------------------------------------------------------------------------------------
 
 """
+import json
 import os
+import shutil
+import subprocess
 import sys
 import torch
+import numpy as np
 import sys_logger
 import nn_model
+import nn_utils
 import nn_pipeline
+import skimage.io as img_sio
+import scipy.io as mat_io
+from scipy.ndimage import distance_transform_edt as d_tran
 
 sys_log = sys_logger.Logger(os.path.basename(__file__)[0:-3]).logger  # logger
 
 if __name__ == '__main__':
     sys_log.debug('Program start')
 
-    # config params
+    # config params (train NN)
     device = 'cuda:0'
     skip_train = False
+    skip_test = False
+    skip_simulation = False
     dataset_path = '../data/dataset32.h5'
     hdf5_config = {
         'rdcc_nslots': 1601,
@@ -59,7 +70,124 @@ if __name__ == '__main__':
                                train_batch=6, epoch=5, device=device, accumulation_steps=5, epoch_save=True, update_count=1)
         torch.save(unet.state_dict(), '../model/unet.pth')
     else:
+        unet = torch.compile(unet)
         unet.load_state_dict(torch.load('../model/unet.pth'))
-        sys_log.info('Read FPN weights from file')
+        sys_log.info('Read Unet weights from file')
+
+    # test NN
+    if not skip_test:
+        # config params (test NN)
+        random_seed = 24
+        np.random.seed(random_seed)
+        p_width = 1920
+        p_height = 1080
+        pdn_voltage = 5.0
+        min_seg_num = 1  # min segments of single edge
+        max_seg_num = 5  # max segments of single edge
+        column = 8  # max columns of json objects
+        precision = 1  # digits precision of json dump
+        simulator_path = 'C:/Users/12416/Desktop/C++File/Project/TECoSim'  # path of TECoSim simulator
+        frame_idx = 1  # index of test frame (start from one)
+
+        target_config_path = simulator_path + '/config/pdn_injection.json'
+        if not skip_simulation:
+            edge_sel = int(np.random.randint(low=1, high=16, size=1)[0])
+            seg_num_arr, seg_len_arr, amount, pdn_row, pdn_col, pdn_vol = (
+                nn_utils.gen_pdn(sel=edge_sel, p_height=p_height, p_width=p_width,
+                                 min_seg_num=min_seg_num, max_seg_num=max_seg_num, pdn_voltage=pdn_voltage))
+            sys_log.info("PDN test case --> "
+                         "left: %d-seg %d-len | right: %d-seg %d-len | "
+                         "up: %d-seg %d-len | down: %d-seg %d-len | "
+                         "total: %d-seg %d-len",
+                         seg_num_arr[0], seg_len_arr[0],
+                         seg_num_arr[1], seg_len_arr[1],
+                         seg_num_arr[2], seg_len_arr[2],
+                         seg_num_arr[3], seg_len_arr[3],
+                         sum(seg_num_arr), sum(seg_len_arr))
+            # Export to json (without using json library)
+            file_path = '../pdn/pdn_injection_test.json'
+            nn_utils.dump_pdn(file_path=file_path, column=column, precision=precision, amount=amount,
+                              pdn_row=pdn_row, pdn_col=pdn_col, pdn_vol=pdn_vol)
+
+            # remove json file
+            try:
+                os.remove(target_config_path)
+                sys_log.debug("PDN test case --> json file %s removed", target_config_path)
+            except Exception as err:
+                sys_log.error("Remove file %s failed with error %s", target_config_path, str(err))
+                sys.exit(0)
+            # copy json to config
+            source_config_path = '../pdn/pdn_injection_test.json'
+            try:
+                shutil.copy(source_config_path, target_config_path)
+                sys_log.debug("PDN test case --> copy json file from %s to %s", source_config_path, target_config_path)
+            except Exception as err:
+                sys_log.error("Copy json file from %s to %s failed with error %s",
+                              source_config_path, target_config_path, str(err))
+                sys.exit(0)
+            # simulation
+            sys_log.debug("PDN test case --> simulation start")
+            result = subprocess.run([simulator_path + '/TECoSim.exe', simulator_path + '/config/'],
+                                    stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            if result.returncode != 0:
+                sys_log.error("Simulator terminated with error")
+                sys.exit(0)
+            else:
+                sys_log.debug("PDN test case --> simulation done")
+        else:
+            sys_log.info("Simulation skipped")
+
+        # read the json
+        with open(target_config_path, 'r', encoding='utf-8') as jf:
+            jdata = json.load(jf)
+            amount = jdata['amount']
+            inj_row = np.array(jdata['row'])
+            inj_col = np.array(jdata['col'])
+            if inj_row.size != amount or inj_col.size != amount:
+                raise Exception("Unmatched row and col of injection point")
+        # get the dmap
+        max_dist = np.sqrt((p_height - 1) ** 2 + (p_width - 1) ** 2)
+        bin_map = np.zeros((p_height, p_width), dtype=np.int32)
+        bin_map[inj_row, inj_col] = 1
+        dmap_slice = np.array(d_tran(1 - bin_map)) / max_dist
+        dmap_slice = dmap_slice.astype(np.float32)  # [H W]
+        # get the img
+        img_path = simulator_path + '/data/frame_input/frame_' + str(frame_idx) + '.png'
+        img_raw = np.array(img_sio.imread(img_path))
+        img_slice = img_raw.astype(np.float32) / 255.0
+        img_slice = img_slice.transpose(2, 0, 1)  # [C H W]
+        # get the vdata
+        vdata_path = simulator_path + '/data/voltage/V_' + str(frame_idx) + '.bin'
+        raw_vdata = np.fromfile(vdata_path, dtype=np.float64)
+        raw_vdata = raw_vdata.reshape((p_height, p_width))
+        vdata_slice = raw_vdata.astype(np.float32) / pdn_voltage  # [H W]
+
+        # get the prediction by NN
+        unet.eval()  # toggle inference mode
+        with torch.no_grad():
+            img_slice_ = torch.from_numpy(img_slice).unsqueeze(0)
+            dmap_slice_ = torch.from_numpy(dmap_slice).unsqueeze(0).unsqueeze(0)
+            vdata_slice_ = torch.from_numpy(vdata_slice).unsqueeze(0).unsqueeze(0)
+            nn_input = torch.cat([img_slice_, dmap_slice_], dim=1).to(device)
+            nn_output = unet(nn_input)
+        v_real = vdata_slice * pdn_voltage  # [H W]
+        v_predict = nn_output.cpu()
+        v_predict = v_predict.numpy()
+        v_predict = np.squeeze(v_predict)  # [H W]
+        v_predict = v_predict * pdn_voltage
+        verr = v_real - v_predict
+        max_err = np.max(np.abs(verr))
+        avg_err = np.mean(np.abs(verr))
+        sys_log.info("Max error: %.4f mV, average error: %.4f mV", 1e3 * max_err, 1e3 * avg_err)
+        data = {'inj_row': inj_row,
+                'inj_col': inj_col,
+                'dmap_slice': dmap_slice,
+                'img_slice': img_slice,
+                'v_real': v_real,
+                'v_predict': v_predict,
+                'v_err': verr}
+        mat_io.savemat('../test/unet_test.mat', data)
+    else:
+        sys_log.info('Unet test skipped')
 
     sys_log.debug('Program end')
